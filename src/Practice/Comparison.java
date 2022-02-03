f02fad4c916cbf0fe37691c869e092f03d2af56e
sandeepvarma8466 <svarma852@gmail.com>
2022-02-03 22:30:47 +0530

    added : message

---
+++ src/Practice/Comparison.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Comparison {
+
+        public static void main(String[] args) {
+            Integer i = new Integer(10);
+            Double d = new Double(10.2011);
+
+            boolean result = i.equals(d);
+            System.out.println(result);
+        }
+    }
+
